7df6a7666ba9987d4521944a133cef56f9ab5727
wanglidan <wangld6543@163.com>
2019-11-18 17:09:23 +0800

    add

---
+++ 目标和分享/1.js
@@ -0,0 +1,20 @@
+
+var obj = new Proxy({}, {
+  get: function(target, key, receiver) {
+    console.log(`getting ${key}!`);
+    return Reflect.get(target, key, receiver);
+  },
+  set: function(target, key, value, receiver) {
+    console.log(`setting ${key}!`);
+    return Reflect.set(target, key, value, receiver);
+  }
+})
+
+obj.count = 1;
+
+obj.count += 1;
+
+// 运行结果：
+// setting count!
+// getting count!
+// setting count!
