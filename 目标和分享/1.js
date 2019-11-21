528a6504664ecbcc373aa7d53f4e435b02861925
wanglidan <wangld6543@163.com>
2019-11-21 14:58:10 +0800

    代理模式

--- 目标和分享/1.js
+++ 目标和分享/1.js
@@ -18,3 +18,27 @@ obj.count += 1;
 // setting count!
 // getting count!
 // setting count!
+
+var mult = function() {
+  console.log('开始计算');
+  let a = 1;
+  for (let i = 0; i < arguments.length; i++) {
+    a = a * arguments[i];
+  }
+  return a;
+}
+
+var proxyMult = (function() {
+  let cache = {};
+  return function() {
+    let args = Array.prototype.join.call(arguments);
+    if (args in cache) {
+      return cache[args];
+    }
+    return cache[args] = mult.apply(this, arguments);
+  }
+})();
+
+console.log(proxyMult(1,3,4));
+console.log(proxyMult(3,4,1));
+console.log(proxyMult(1,3,4));
